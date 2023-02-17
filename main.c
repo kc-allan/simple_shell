e86477f0ba7a24d5ddd3b76f2a812df4bc8210a3
allan <allancheruiyot003@gmail.com>
2023-02-17 01:47:33 -0800

    ALX SE

--- main.c
+++ main.c
@@ -1,20 +1,59 @@
 #include "main.h"
 /**
- * main - entry point
- * @argc: argument count
- * @argv: array of arguments
+ * execute_command - runs commamd provided to the shell
+ * @argv: array of argument variables
  * @envp: environment variables
+ *
+ * Return: void
+ */
+void execute_command(char **argv, char **envp)
+{
+	char *path = "/usr/bin/", *cmd;
+	pid_t pid;
+	int status;
+	size_t cmd_size = (strlen(path) + strlen(argv[0]) + 1);
+	
+	setenv("PATH", path, 1);
+	if (access(path, F_OK) == -1)
+	{
+		printf("%s: command not found\n", argv[0]);
+		return;
+	}
+	cmd = malloc(cmd_size);
+	strcpy(cmd, path);
+	strcat(cmd, argv[0]);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (execve(cmd, argv, envp) == -1)
+		{
+			printf("./hsh: 1: %s: not found\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(&status);
+	free(cmd);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @envp: environment variables
+ *
  * Return: 0 (Success)
  */
-int main(int argc, char **argv, char **envp)
+int main(int argc, char **envp)
 {
-	char *command = NULL, *cmd;
+	char *command = NULL, **argv;
 	ssize_t bytes_read;
 	size_t len = 0;
-	pid_t pid;
-	int status;	
-	char *path = "/usr/bin/";
-
+	
 	while (argc == 1)
 	{
 		printf("($) ");
@@ -34,33 +73,8 @@ int main(int argc, char **argv, char **envp)
 		if (command[bytes_read - 1] == '\n')
 			command[bytes_read - 1] = '\0';
 		argv = get_token(command);
-		setenv("PATH", path, 1);
-		if (access(path, F_OK) == -1)
-		{
-			printf("%s: command not found\n", command);
-			continue;
-		}
-		cmd = malloc(MAX_LEN);
-		strcpy(cmd, path);
-		strcat(cmd, argv[0]);
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			free(command);
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0)
-		{
-			if (execve(cmd, argv, envp) == -1)
-			{
-				printf("./hsh: 1: %s: not found\n", command);
-				free(command);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-			wait(&status);
+		execute_command(argv, envp);
 	}
+	free(command);
 	return (0);
 }
